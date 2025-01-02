854fe60655f76dc4483cf890857fb7ab620c7791
HMAMOU Soukaina <135372189+HMAMOUSoukaina@users.noreply.github.com>
2025-01-02 20:44:42 +0100

    Update HelloWorld.java5555555555

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
         System.out.println("Bonjour, Monde from Jenkins 2222222222222222222222!");
         System.out.println("Bonjour, Monde from Jenkins!3333333333333333333333333333333333333333333");
         System.out.println("Bonjour, Monde from Jenkins!444444444444");
+        System.out.println("Bonjour, Monde from Jenkins!555555555555555");
         
     }
 }
